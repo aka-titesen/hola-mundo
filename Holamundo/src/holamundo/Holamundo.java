2856814141c7f400d37af221ba6c50e88daf648a
aka-titesen <fngz7788@gmail.com>
2022-10-10 11:59:35 -0300

    Ésto es el log in

--- Holamundo/src/holamundo/Holamundo.java
+++ Holamundo/src/holamundo/Holamundo.java
@@ -4,6 +4,8 @@ public class Holamundo {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        
+        System.out.println("Ésto es el log in");
     }
 
 }
